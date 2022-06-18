246ceeb8aec60da1453aa7198f571edf67b75f48
OmerNouri2 <omernn@gmail.com>
2022-06-18 17:18:27 +0300

    start of sys call

--- Makefile
+++ Makefile
@@ -132,6 +132,8 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
+	$U/_symlink\
+	$U/_readlink\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
--- kernel/defs.h
+++ kernel/defs.h
@@ -104,6 +104,8 @@ void            yield(void);
 int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
+int             symlink ( const char ∗ oldpath , const char ∗ newpath );
+int             readlink ( const char ∗ pathname , char ∗ buf , int bufsize );
 
 // swtch.S
 void            swtch(struct context*, struct context*);
